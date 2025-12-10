903af92c01e66c105fb5342985108432f51ecd40
lucg-gith <garrelucia@gmail.com>
2025-12-10 20:49:15 +0000

    add: rate limit

--- backend_utn/src/routes/authRouter.ts
+++ backend_utn/src/routes/authRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import AuthController from "../controllers/authController";
+import limiter from "../middleware/rateLimitMiddleware";
 
 const authRouter = Router();
 
 // http://localhost:3000/auth/register
-authRouter.post("/register", AuthController.register);
+authRouter.post("/register", limiter, AuthController.register);
 // http://localhost:3000/auth/login
-authRouter.post("/login", AuthController.login);
+authRouter.post("/login", limiter, AuthController.login);
 
 export default authRouter;
